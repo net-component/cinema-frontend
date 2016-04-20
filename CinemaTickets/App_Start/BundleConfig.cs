7ba84bcaf3c39db92e9f9384a71e2b47c3628723
Pongrácz Lajos-Levente <pongraczlajoslevente@gmail.com>
2016-04-20 09:00:34 +0300

    Movies front-end initial.

---
+++ CinemaTickets/App_Start/BundleConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Optimization;
+
+namespace Cinema.Tickets
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/knockout.js")
+                .Include("~/Scripts/knockout-{version}.js"));
+        }
+    }
+}
---
+++ CinemaTickets/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Cinema.Tickets.MvcApplication" Language="C#" %>
